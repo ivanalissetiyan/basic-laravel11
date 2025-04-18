76f2ebe959adc8dc14b5d8a91320063ca92983c4
ivanalissetiyan <ivanalissetiyan1998@gmail.com>
2025-04-19 00:29:39 +0700

    Blade Component

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,20 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Contact</title>
-    <link rel="stylesheet" href="css/style.css">
-
-</head>
-<body>
-    <a href="/">Home</a>
-    <a href="/blog">Blog</a>
-    <a href="/about">About</a>
-    <a href="/contact">Contact Us</a>
-    <h1>Contact Us</h1>
-   <ul>
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <ul>
     <li>
         <a href="https://instagram.com/ivanalissetiyan">Instagram</a>
     </li>
@@ -25,6 +11,9 @@
         <a href="https://facebook.com/ivanalissetiyan">Facebook</a>
     </li>
    </ul>
+</x-layout>
 
-</body>
-</html>
+   
+
+
+
--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,18 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', ['title' => 'Home Page']);
 });
 
 Route::get('/blog', function () {
-    return view('blog',);
+    return view('blog', ['title' => 'Blog']);
 });
 
 Route::get('/about', function () {
-    return view('about');
+    return view('about', ['title' => 'About', 'name' => 'Ivan Alissetiyan']);
 });
 
 Route::get('/contact', function () {
-    return view('contact');
+    return view('contact', ['title' => 'Contact']);
 });
 
